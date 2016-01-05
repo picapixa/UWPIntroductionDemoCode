5706daea52cd356eb032cb07e41297f7206ff3d3
Raffy Alcoriza <jrpalcoriza@outlook.com>
2016-01-05 16:59:18 +0800

    Added solution files.

---
+++ 100Win10Intro-Demo/ProfilePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace _100Win10Intro_Demo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ProfilePage : Page
+    {
+        public ProfilePage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            object parameter = e.Parameter;
+        }
+
+        private async void btn_changeName_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialogResult cd_res = await ChangeDialog.ShowAsync();
+            if (cd_res == ContentDialogResult.Primary)
+            {
+                textBlock.Text = tbx_name.Text;
+            }
+
+        }
+    }
+}
